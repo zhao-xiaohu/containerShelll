b63d44af7d52f1387ca8a38ddf82976aa30873b8
Francis <francisshi@mindverse.ai>
2023-02-27 21:32:28 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Script from "./Script";
-// import WebApp from "./WebApp";
+// import Script from "./Script";
+import WebApp from "./WebApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
