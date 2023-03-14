fcc4dbf023a5a6a7d43ce7fc24b78866bf752c04
Francis <francisshi@mindverse.ai>
2023-03-14 20:46:57 +0800

    update

--- src/WebApp.js
+++ src/WebApp.js
@@ -1,8 +1,7 @@
 /**
  * 业务接入样例
  */
-import { useEffect } from "react";
-import App from "@mindverse/container";
+import App, { DevelopType } from "@mindverse/container";
 
 export default function Container(props) {
   const refUserId = "shitou";
@@ -13,35 +12,40 @@ export default function Container(props) {
         sessionCb={(_sessionId) => {}}
         config={{
           mindConfig: {
-            mindId: "76643513529405440",
+            mindId: "81870359162392576", // pre
+            // mindId: '76643513529405440', // test
             mindType: "original",
           },
           socketConfig: {
-            apiVersion: "1.2.0",
-            platform: "Saas",
-            appId: "os_742e9fcd-d543-4c99-94d7-404119bea18a",
-            bizType: "SAAS",
-            merchantId: "c1e3x",
-            mAuthType: "SAAS_KEY",
+            apiVersion: "1.3.0",
+            platform: "web",
+            appId: "os_54b9f83c-58e2-4e32-8cc8-b1dcb872c0aa", // pre
+            // appId: 'os_742e9fcd-d543-4c99-94d7-404119bea18a', // test
+            bizType: "",
+            merchantId: "c1dyf", // pre
+            // merchantId: 'c1e3x', // test
+            mAuthType: "STATION_KEY",
 
             refUserId,
 
-            merchantBaseURL: "https://test-accessor.mindverse.com",
-            merchantSocketPath: "/rest/demo/ws/create",
-            merchantSessionOpenPath: "/rest/demo/session/create",
-            merchantSessionClosePath: "/rest/demo/session/close",
-            merchantUserRegisterPath: "/rest/demo/user/register",
-            merchantSocketCheckPath: "/rest/demo/ws/get",
-            merchantSessionCheckPath: "/rest/demo/session/get",
+            merchantBaseURL: "https://gateway-pre.mindverse.com", // pre
+            // merchantBaseURL: 'https://gateway-test.mindverse.com', // test
+            merchantSocketPath: "/chat/rest/general/ws/create",
+            merchantSessionOpenPath: "/chat/rest/general/session/create",
+            merchantSessionClosePath: "/chat/rest/general/session/close",
+            merchantUserRegisterPath: "/chat/rest/general/user/register",
+            merchantSocketCheckPath: "/chat/rest/general/ws/get",
+            merchantSessionCheckPath: "/chat/rest/general/session/get",
 
             headers: {},
           },
           userConfig: {
-            userName: "shitou-demo", // document.getElementById("#nickname").value,
+            userName: "shitou-demo",
             avatar:
-              "https://cdn.mindverse.com/img/zzzz202302211676948571901%E5%BF%83%E8%AF%86%E5%BC%95%E5%AF%BC%E5%91%98.png",
+              "https://cdn.mindverse.com/files/zzzz20230308167826913484720230308-175144.gif",
           },
           dynamicHeight: false,
+          developType: DevelopType.SCRIPT,
         }}
       />
     </div>
