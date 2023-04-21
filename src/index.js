e285392344adb580679c0e4d4c1542658f08d4b9
Francis <francisshi@mindverse.ai>
2023-04-21 15:08:25 +0800

    script body css text

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import Script from "./Script";
 import WebApp from "./WebApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Script /> */}
     <WebApp />
   </React.StrictMode>
 );
